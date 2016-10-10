385073aa91e251dc7e58d038370dbd7a21fb4b75
Jason Carter <jason.ian.carter@gmail.com>
2016-10-10 12:30:29 -0400

    Added tracking and new notebook

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -2,24 +2,72 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from collections import Counter
+
+# Track results of trials
+results = Counter()
+
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-
     def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        super(LearningAgent, self).__init__(
+            env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+
         # TODO: Initialize any additional variables here
         random.seed()  # initialize number generator
-        self.actions = [None, 'forward', 'left', 'right']
-        # TODO: setup possible states
-        self.state = {}
+        self.state = None
+        self.qtable = {}
+        self.alpha = 0.2  # learning rate
+        self.epsilon = 0.1  # exploration rate
+        self.gamma = 0.4  # discount factor
+        self.total_reward = 0
+        self.last_reward = 0
+        self.last_action = None
+        self.last_state = None
+        self.valid_actions = Environment.valid_actions
+        # self.results = Counter()
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
+
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.state = None
+        self.total_reward = 0
+        self.last_reward = 0
+        self.last_action = None
+        self.last_state = None
+
+    def get_action(self):
+        if random.random() > self.epsilon and (self.state in self.qtable):
+            # assign max Q value for current state
+            max_q = self.qtable[self.state].index(max(self.qtable[self.state]))
+            # assign action based on max Q value
+            action = self.valid_actions[max_q]
+        else:
+            action = random.choice(self.valid_actions)
+
+        return action
+
+    def update_policy(self, action, reward):
+        if self.state not in self.qtable:
+            self.qtable[self.state] = [0, 0, 0, 0]
+
+        if self.last_state not in self.qtable:
+            self.qtable[self.last_state] = [0, 0, 0, 0]
+
+        # Update q table based on the Q-learning =>
+        # Q(state, action) = alpha(r + gamma * max[Q(next state, actions)] - Q[s,a])
+
+        self.qtable[self.state][self.valid_actions.index(action)] = \
+            (
+                self.qtable[self.last_state][self.valid_actions.index(self.last_action)] +  # Q[s,a] +
+                self.alpha * (reward + self.gamma * max(self.qtable[self.state]) -  # alpha ( r+gamma*maxQ[s',a'] -
+                              self.qtable[self.last_state][self.valid_actions.index(self.last_action)])  # Q[s,a])
+            )
 
     def update(self, t):
         # Gather inputs
@@ -28,19 +76,36 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        self.state = inputs
-        self.state["next_waypoint"] = self.next_waypoint
-        
+        # input['right'] is not required
+        self.state = (inputs['light'], inputs['oncoming'], inputs['left'], self.next_waypoint)
+
         # TODO: Select action according to your policy
-        action = self.actions[random.randint(0, 3)]
+        action = self.get_action()
 
         # Execute action and get reward
         reward = self.env.act(self, action)
+        results['penalties'] += reward if reward < 0 else 0
+        self.total_reward += reward
 
         # TODO: Learn policy based on state, action, reward
+        self.update_policy(action, reward)
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-        print self.state
+        self.last_action = action
+        self.last_reward = reward
+        self.last_state = self.state
+
+        # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}" \
+        #     .format(deadline, inputs, action, reward)  # [debug]
+
+        # Track results
+        if self.env.done:  # if destination reached
+            results['win'] += 1
+            results['reward'] += self.total_reward
+            # print 'Results {}'.format(self.results)
+        elif deadline == 0:
+            results['lose'] += 1
+            results['reward'] += self.total_reward
+            # print 'Results {}'.format(self.results)
 
 
 def run():
@@ -49,16 +114,19 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=False)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.05,
+                    display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=100)  # run for a specified number of trials
+    sim.run(n_trials=10)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
 if __name__ == '__main__':
     run()
+
+print 'Results {}'.format(results)
--- smartcab/report.md
+++ smartcab/report.md
@@ -17,11 +17,11 @@ Observe what you see with the agent's behavior as it takes random actions.
 Does the smartcab eventually make it to the destination? Are there any other interesting observations to note?
 
 **Answer:**
-The angent's behaviour is, unsurprisingly, random. It would at times move in a circle (around the block) not really progressing 
-in any net new direction. And it doesn't appear to have any logic (it's random) behind its movement as rewards for previous states and actions are ignored.
 
-However, the smartcab does eventually make it to its destination.
-TODO: Add some other stuff here + Can I do some stats description
+The angent's behaviour is, unsurprisingly, random. It would at times move in a circle (around the block) not really progressing 
+in any net new direction. And it doesn't appear to have any logic behind its movement as rewards for previous states and actions are ignored.
+Another item of the agent's behavior is the agent can also drive over the right edge of the world and turn up on the left edge of the world.
+However, the smartcab does eventually make it to its destination on occasion.
 
 ## Inform the Driving Agent
 Now that your driving agent is capable of moving around in the environment, 
@@ -40,12 +40,31 @@ Why do you believe each of these states to be appropriate for this problem?
 
 **Answer:**
 
+At each state or waypoint we perceive 6 variables, left, right, oncoming, light, next_waypoint and deadline.
+Since traffic coming from the right, doesn't affect the smartcab's decision, this variable was removed - e.g. if the 
+light is green, traffic from the right has stopped and if light is red, smartcab can only go right and any traffic from the right
+doesn't affect these options. Likewise, the input deadline was not utilized because at this point it does not provide
+any significant information to the smartcab.
+
+Each state identifies a situation and option for the smartcab, which results in a particular reward. With Q-learning implemented
+the smartcab will be able to use these states to learn and best determine its path to the destination.
+ 
+Final states used - light, oncoming, left, next_waypoint 
+
 **OPTIONAL:** 
 
 How many states in total exist for the smartcab in this environment? 
 Does this number seem reasonable given that the goal of Q-Learning is to learn and make informed decisions about each state? Why or why not?
 
 **Answer:**
+
+Light: Red or green
+Oncoming: None, left, right, forward
+Left: None, left, right, forward
+Next_waypoint: None, left, right, forward
+
+Capturing 4 inputs per state with each input having a variation between 2-4, gives us 128 states in total that exist for the smartcab in this environment.
+
 
 ## Implement a Q-Learning Driving Agent
 With your driving agent being capable of interpreting the input information and having a mapping of environmental states, 
@@ -56,14 +75,22 @@ Each action taken by the smartcab will produce a reward which depends on the sta
 The Q-Learning driving agent will need to consider these rewards when updating the Q-values. Once implemented, 
 set the simulation deadline enforcement enforce_deadline to True. Run the simulation and observe how the smartcab moves about the environment in each trial.
 
-The formulas for updating Q-values can be found in this video.
-
 **QUESTION:**
 
 What changes do you notice in the agent's behavior when compared to the basic driving agent when random actions were always taken? 
 Why is this behavior occurring?
 
 **Answer:**
+
+At the beginning of trials the agent's behavior remains the same; however, as trials proceed, the agent no longer seems to act randomly but its movement
+tends towards the destination.
+
+Behvaior - learning from it's past experience, Q table starts off empty and as the agent explores its world each state, action and its reward,
+it's q value, is stored. As more q values are stored the agent becomes at choosing the best action.
+
+With no reward for making it to the destination sooner rather than later, the agent attempts to maximize reward following waypoints 
+that may not result in the shortest path to the destination.
+
 
 ## Improve the Q-Learning Driving Agent
 Your final task for this project is to enhance your driving agent so that, after sufficient training, 
@@ -83,6 +110,28 @@ This task is complete once you have arrived at what you determine is the best co
 
 Report the different values for the parameters tuned in your basic implementation of Q-Learning. 
 For which set of parameters does the agent perform best? How well does the final driving agent perform?
+
+No. of Trials = 10, alpha = 0.5, epsilon = 0.5, gamma = 0.4
+Reached Destination | Ran out of Time
+--- | ---
+31 | 19
+30 | 20
+35 | 15
+27 | 23
+33 | 17
+
+No. of Trials = 10, alpha = 0.1, epsilon = 0.2, gamma = 0.3
+Reached Destination | Ran out of Time
+--- | ---
+35 | 15
+41 | 9
+37 | 13
+35 | 15
+40 | 10
+
+No. of Trials = 10, alpha = 0.1, epsilon = 0.2, gamma = 0.3
+
+
 
 **QUESTION:**
 Does your agent get close to finding an optimal policy, i.e. reach the destination in the minimum possible time, 
